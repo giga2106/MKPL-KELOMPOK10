5f5ed655bf5a65cae066ab6bc8dca6ac50f47f3a
Giga <giga2106@gmail.com>
2019-12-06 21:16:46 +0700

    Online Shop v3.0
    Fungsi:
    -LIhat barang terlaris

--- POPO/application/modules/Home/views/Home_peralatan_v.php
+++ POPO/application/modules/Home/views/Home_peralatan_v.php
@@ -41,45 +41,43 @@
   <!--content-->
 <div class="content">
   <!--Products-->
-  <div class="latest-w3">
-    <div class="container">
-      <h3 class="tittle1">Kategori Yang Sering Dicari</h3>
-      <div class="latest-grids">
-        <div class="col-md-4 latest-grid">
-          <div class="latest-top">
-            <img  src="<?php echo base_url()."assets/"; ?>mainpage/images/kat1.jpg" class="img-responsive"  alt="">
-            <a href="<?php echo base_url()."Peralatan/Kategori/2"; ?>">
-              <div class="latest-text">
-                <h4>Sepatu Fulsal</h4>
+
+
+  <!--akhir promo-->
+    <div class="new-arrivals-w3agile">
+      <div class="container">
+        <h3 class="tittle1">Peralatan Terbaru</h3>
+        <div class="arrivals-grids">
+          <?php
+          foreach ($data_peralatan as $key => $pecah) {
+
+           ?>
+          <div class="col-md-3 arrival-grid simpleCart_shelfItem">
+            <div class="grid-arr">
+              <div  class="grid-arrival">
+                <figure>
+                  <a href="<?php echo base_url()."Peralatan/Detail/"; ?><?php echo $pecah->id_peralatan ?>" class="new-gri" >
+                    <div class="grid-img">
+                      <img src="<?php echo base_url()."foto_peralatan/"; ?><?php echo $pecah->foto_peralatan; ?>" class="img-responsive" alt="">
+                    </div>
+                  </a>
+                </figure>
               </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4 latest-grid">
-          <div class="latest-top">
-            <img  src="<?php echo base_url()."assets/"; ?>mainpage/images/kat3.jpg" class="img-responsive"  alt="">
-            <a href="<?php echo base_url()."Peralatan/Kategori/4"; ?>">
-              <div class="latest-text">
-                <h4>Jersey Sepak Bola</h4>
+              <div class="women">
+                <h6 ><a class="judul" href="<?php echo base_url()."Peralatan/Detail/"; ?><?php echo $pecah->id_peralatan ?>"><?php echo $pecah->nama_peralatan; ?></a></h6>
+                <p ><em class="item_price format-number" >Rp <?php echo number_format($pecah->harga_peralatan); ?></em></p>
+                <a href="<?php echo base_url()."Peralatan/Detail/"; ?><?php echo $pecah->id_peralatan ?>" data-text="Add To Cart" class="add ">Lihat detail</a>
               </div>
-            </a>
+            </div>
           </div>
+          <?php //if (++$i == 4) break;
+        } ?>
+          <div class="clearfix"></div>
+          <br><br><br><br>
+          <h3><a class="add" href="<?php echo base_url()."Peralatan/"; ?>">Lihat Semua Peralatan</a></h3>
         </div>
-        <div class="col-md-4 latest-grid">
-          <div class="latest-top">
-            <img  src="<?php echo base_url()."assets/"; ?>mainpage/images/kat2.jpg" class="img-responsive"  alt="">
-            <a href="<?php echo base_url()."Peralatan/Kategori/1"; ?>">
-              <div class="latest-text">
-                <h4>Sepatu Sepak Bola</h4>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="clearfix"></div>
       </div>
-      <!-- <div class="latest-grids"> -->
-        <!-- <div class="clearfix"></div> -->
-      <!-- </div> -->
     </div>
+  <!--new-arrivals-->
 </div>
 <!--content-->
